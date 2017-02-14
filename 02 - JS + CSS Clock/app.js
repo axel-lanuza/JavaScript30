7b3d9d5d09c6e925b14adfbeefc0b06cf48bea04
Axel Lanuza <axel-lanuza@definityfirst.com>
2017-02-14 15:37:46 -0600

    Watch complete and start 3rd project

--- 02 - JS + CSS Clock/app.js
+++ 02 - JS + CSS Clock/app.js
@@ -1,12 +1,41 @@
-let ClockApp = (function (window, undefined) {
+let ClockApp = (function(window, undefined) {
 
-  let init = function () {
-      console.log("init")
-  };
+    //Dom Cache
+    const domCache = {
+        secondsHand : document.querySelector('.second-hand'),
+        minutesHand : document.querySelector('.min-hand'),
+        hoursHand : document.querySelector('.hour-hand')
+      }
 
-  return {
-    Init: init
-  }
+    const init = function() {
+        console.log("init");
+        bindEvents();
+
+    };
+
+    let bindEvents = function() {
+        setInterval(setDate, 1000);
+        console.log(domCache.secondsHand);
+    }
+
+    let setDate = function() {
+        const now = new Date();
+        const seconds = now.getSeconds();
+        const minutes = now.getMinutes();
+        const hours = now.getHours();
+        const secondsToDegrees = ((seconds / 60) * 360) + 90;
+        const minutesToDegress = ((minutes / 60) * 360) + 90;
+        const housrToDegrees = ((hours / 12) * 360) + 90;
+
+        domCache.secondsHand.style.transform = `rotate(${secondsToDegrees}deg)`;
+        domCache.minutesHand.style.transform = `rotate(${minutesToDegress}deg)`;
+        domCache.hoursHand.style.transform = `rotate(${housrToDegrees}deg)`;
+    }
+
+    return {
+        Init: init
+    }
+
 })(window, undefined);
 
 let callback = ClockApp.Init();
